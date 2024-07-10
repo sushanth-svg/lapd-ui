cbf3ba34b02a7f41b34acc6c509d97d003b047da
Yuvraj Gupta <mynameisyuvraj2004@gmail.com>
2024-07-10 12:22:12 +0530

    changed female voice

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,22 @@
           <div class="audio_section p-3">
             <img src="/src/assets/logo.svg" alt="LAPD" class="LAPD_logo">
             <div class="LAPD_name">
-              <h3>Los <br> Angeles <br>
-                Police<br>
-                Department</h3>
+              <h3>Crista 911</h3>
             </div>
             <img src="/src/assets/js/mic.svg" alt="LAPD" class="Mic">
-
+ 
             <div class="row">
               <div class="col-lg-12">
                 <div class="text-center">
                   <div class="audio_icon my-5 pt-5">
                     <font-awesome-icon icon="fa-solid fa-microphone" />
                   </div>
-
+ 
                   <button type="button" @click="startListening" :disabled="isRecording" class="btn start_btn mb-4">Start
                     Recording</button>
                   <!-- <button type="button" @click="stopListening" :disabled="recordingStop" class="btn start_btn mb-4">Stop
                     Recording</button> -->
-
+ 
                 </div>
               </div>
             </div>
@@ -40,7 +38,7 @@
                   <a>
                     <img class="exc me-4" src="/src/assets/images/delete_icon.svg" alt="Delete">
                   </a>
-
+ 
                   <div class="search_box">
                     <input type="text" class="form-control" placeholder="Search" aria-describedby="emailHelp" />
                     <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="search-icon" />
@@ -52,28 +50,39 @@
               <div class="col-lg-12">
                 <div class="chat_box">
                   <div class="row">
-                    <div class="col-lg-12" v-for="(items, index) in chatJson" :key="index">
 
 
 
-
-                      <div class="user_chat d-flex align-items-star mb-4">
+                    <div class="col-lg-12">
+                      <div class="lapd_chat d-flex align-items-start justify-content-end mb-4">
+                        <div class="chat_text_div d-flex flex-column align-items-end">
+                          <p>Welcome to 911 emergency, how can i help you?</p>
+                        </div>
                         <div class="img-box">
+                          <img src="/src/assets/images/lapd_chat.png" alt="LAPD">
+                        </div>
+                      </div>
+                    </div>
+
+ 
+                    <div class="col-lg-12" v-for="(items, index) in chatJson" :key="index">
+                      <div class="user_chat d-flex align-items-star mb-4">
+                        
+                        <div class="img-box" v-if="items.transcription">
                           <img src="/src/assets/images/user-chat.png" alt="User">
                         </div>
-
                         <div class="chat_text_div" v-if="items.transcription">
                           <p>Language detected: {{ items.detectedlanguage }} </p>
                           <p> <b>{{ items.detectedlanguage }} : </b>{{ items.transcription }} </p>
                           <p> <b>English : </b>{{ items.translatedtext }}</p>
                         </div>
-
-
+ 
+ 
                       </div>
-
-
-
-
+ 
+ 
+ 
+ 
                       <div v-if="items.convertedtext"
                         class="lapd_chat d-flex align-items-start justify-content-end mb-4">
                         <div class="chat_text_div d-flex flex-column align-items-end">
@@ -93,41 +102,33 @@
                             </span>
                           </div>
                           </p>
-
-
+ 
+ 
                         </div>
                         <div class="img-box">
                           <img src="/src/assets/images/lapd_chat.png" alt="LAPD">
                         </div>
                       </div>
-
-
-
-
-
-
-
-
-
-
+ 
+ 
                       <!-- <div class="user_chat d-flex align-items-star mb-4">
                                   <div class="img-box">
                                     <img src="/src/assets/images/user-chat.png" alt="User">
                                   </div>  
-                                
+                               
                                   <div class="chat_text_div" v-if="transcription">
                                     <p>Language detected: {{detectedlanguage}} </p>
                                     <p> <b>{{detectedlanguage}} : </b>{{transcription}} </p>
                                   <p> <b>English : </b>{{translatedtext}}</p>
                                   </div>
-
+ 
                                   <div class="chat_text_div" v-if="processing">
                                     <p> Processing.... </p>
                                     </div>
                       </div>
-
-
-
+ 
+ 
+ 
                  
                       <div v-if="openai" class="lapd_chat d-flex align-items-start justify-content-end mb-4">
                               <div class="chat_text_div d-flex flex-column align-items-end">
@@ -145,41 +146,41 @@
                                       <font-awesome-icon icon="fa-solid fa-rotate-right" @click="stopAudio(convertedtext)" />
                                     </span>
                                   </div>
-                                </p> 
-                              
-
+                                </p>
+                             
+ 
                               </div>
                               <div class="img-box">
                                 <img src="/src/assets/images/lapd_chat.png" alt="LAPD">
                               </div>  
                       </div>
                     -->
-
-
-
-
+ 
+ 
+ 
+ 
                     </div>
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
                     <div class="user_chat d-flex align-items-star mb-4" v-if="processing">
                       <div class="img-box">
                         <img src="/src/assets/images/user-chat.png" alt="User">
                       </div>
-
+ 
                       <div class="chat_text_div">
                         Processing...
                       </div>
-
-
+ 
+ 
                     </div>
-
-
-
+ 
+ 
+ 
                   </div>
-
+ 
                 </div>
               </div>
             </div>
@@ -189,17 +190,17 @@
     </div>
   </section>
 </template>
-
-
+ 
+ 
 <style scoped></style>
-
-
+ 
+ 
 <script>
 import axios from 'axios';
 import { COMMON_API_HTTP } from './api/axiosConfig';
 import { withDirectives } from 'vue';
-
-
+ 
+ 
 export default {
   data() {
     return {
@@ -225,12 +226,19 @@ export default {
       audioStream: null
     };
   },
-
-
+ 
+ 
   created: function () {
+    // this.chatJson.push({
+    //   openai: "Welcome to 911 emergency, how can i help you?",
+    //   convertedtext: "Welcome to 911 emergency, how can i help you?",
+    //   openairesponse: ["Welcome to 911 emergency, how can i help you?"]
+    // });
+    // console.log(this.chatJson);
     this.stopAudio()
+    this.playAudio("Welcome to 911 emergency, how can i help you?","en","Welcome to 911 emergency, how can i help you?");
   },
-
+ 
   methods: {
     clearFields() {
       this.transcription = '';
@@ -250,7 +258,7 @@ export default {
         this.recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
         this.recognition.continuous = true;
         this.recognition.interimResults = false;
-
+ 
         this.recognition.onresult = (event) => {
           clearTimeout(this.silenceTimeout);
           this.silenceTimeout = setTimeout(() => {
@@ -260,16 +268,16 @@ export default {
             this.stopRecording();
           }, 1000); // Stop after 1 seconds of silence
         };
-
+ 
         this.recognition.onend = () => {
           if (this.isRecording) {
             this.startRecording(); // Restart recording if still in recording mode
           }
         };
-
+ 
         this.recognition.start();
         this.startRecording();
-
+ 
       } catch (err) {
         console.error('Error accessing media devices.', err);
       }
@@ -279,23 +287,23 @@ export default {
       // this.recordingStop = false;
       this.mediaRecorder = new MediaRecorder(this.audioStream);
       this.mediaRecorder.start();
-
+ 
       this.mediaRecorder.ondataavailable = (event) => {
         this.audioChunks.push(event.data);
       };
-
+ 
       this.mediaRecorder.onstop = () => {
         this.audioBlob = new Blob(this.audioChunks, { type: 'audio/wav' });
         this.audioChunks = [];
-
+ 
         this.uploadRecording();
       };
-
+ 
     },
     stopRecording() {
       if (this.mediaRecorder && this.mediaRecorder.state !== 'inactive') {
         this.mediaRecorder.stop();
-
+ 
       }
     },
     stopListening() {
@@ -310,13 +318,13 @@ export default {
       } else {
         this.chathistoryjsonString = JSON.stringify("");
       }
-
+ 
       if (!this.audioBlob) return;
-
+ 
       const formData = new FormData();
       formData.append('file', this.audioBlob, 'audio.wav');
       formData.append('chathistory', this.chathistoryjsonString);
-
+ 
       try {
         const response = await COMMON_API_HTTP.post('/transcribe/', formData, {
           headers: {
@@ -325,17 +333,17 @@ export default {
         });
         this.processing = false;
         console.log(response);
-
+ 
         this.transcription = response.data.transcription;
         this.translatedtext = response.data.translatedtext;
         this.openai = response.data.openai;
         this.detectedlanguage = response.data.detectedlanguage;
         this.convertedtext = response.data.convertedtext;
-
+ 
         console.log(this.openai);
         this.openairesponse = this.openai.split('\n\n');
         console.log(this.openairesponse);
-
+ 
         this.chatJson.push({
           transcription: this.transcription,
           translatedtext: this.translatedtext,
@@ -364,31 +372,77 @@ export default {
           console.log('Please enter some text');
           return;
         }
-
+ 
         if (this.isPaused && this.speechSynthesisUtterance) {
           window.speechSynthesis.resume();
           this.isPaused = false;
           return;
         }
-
+ 
         this.speechSynthesisUtterance = new SpeechSynthesisUtterance(text);
+
+        if (AiEnglishResponse.includes('We have redirected your call to')) {
+            console.log("Reached line 381")
+        }
+ 
+        // Retrieve the list of available voices
+        let voices = window.speechSynthesis.getVoices();
+ 
+        while (voices.length === 0) {
+          voices = window.speechSynthesis.getVoices();
+          await new Promise(resolve => setTimeout(resolve,1000));
+        }
+ 
+        //Find a female voice
+        const ziraVoice = voices.find(voice => voice.name === 'Microsoft Zira - English (United States)');
+
+    if (ziraVoice) {
+    this.speechSynthesisUtterance.voice = ziraVoice;
+  }   else {
+    console.log('Microsoft Zira voice not found, using default voice');
+}
+
+
+
+
         const audioContext = new (window.AudioContext || window.webkitAudioContext)();
         const mediaStreamDestination = audioContext.createMediaStreamDestination();
         const mediaRecorder = new MediaRecorder(mediaStreamDestination.stream);
         let audioChunks = [];
-
+ 
         mediaRecorder.ondataavailable = (event) => {
           audioChunks.push(event.data);
         };
-
+ 
         mediaRecorder.onstop = () => {
           const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
           this.audioUrl = URL.createObjectURL(audioBlob);
         };
+ 
+        if (AiEnglishResponse.includes('We have redirected your call to')) {
+          console.log(this.speechSynthesisUtterance)
+          console.log("Reached line 418")
+        }
+
+
 
         mediaRecorder.start();
+
+        if (AiEnglishResponse.includes('We have redirected your call to')) {
+            console.log("Reached line 426")
+            console.log(this.speechSynthesisUtterance)
+        }
+
+
         window.speechSynthesis.speak(this.speechSynthesisUtterance);
 
+        
+        if (AiEnglishResponse.includes('We have redirected your call to')) {
+            console.log("Reached line 434")
+        }
+
+
+ 
         this.speechSynthesisUtterance.onend = () => {
           // mediaRecorder.stop();
           // this.isRecording = false;
@@ -397,7 +451,7 @@ export default {
             this.startListening();
           }
         };
-
+ 
       } catch (error) {
         console.log(error);
       }
@@ -417,16 +471,16 @@ export default {
       if (this.mediaRecorder && this.mediaRecorder.state === 'recording') {
         this.mediaRecorder.stop();
         this.startListening()
-
+ 
       }
       this.isPaused = false;
-
+ 
     }
   }
-
-
+ 
+ 
   //   methods: {
-
+ 
   // clearFields(){
   //       this.transcription= ''
   //       this.translatedtext=''
@@ -436,83 +490,83 @@ export default {
   //       this.convertedtext=''
   //       this.speechSynthesisUtterance= null
   //       this.chatJson=[]
-
+ 
   // },
-
+ 
   //     async startRecording() {
-
+ 
   //      // this.clearFields()
-
+ 
   //       try {
-
+ 
   //         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
   //         this.mediaRecorder = new MediaRecorder(stream);
   //         this.mediaRecorder.start();
-
+ 
   //         this.mediaRecorder.ondataavailable = (event) => {
   //           this.audioChunks.push(event.data);
   //         };
-
+ 
   //         this.mediaRecorder.onstop = () => {
   //           this.audioBlob = new Blob(this.audioChunks, { type: 'audio/wav' });
   //           this.audioChunks = [];
   //           this.uploadRecording()
   //         };
-
+ 
   //         this.isRecording = true;
-
-
+ 
+ 
   // // Initialize SpeechRecognition
   //       this.recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
   //         this.recognition.continuous = true;
   //         this.recognition.interimResults = false;
-
-
+ 
+ 
   //         this.recognition.onresult = (event) => {
   //           clearTimeout(this.silenceTimeout);
   //           this.silenceTimeout = setTimeout(() => {
   //             this.stopRecording();
   //           }, 2000); // Stop after 2 seconds of silence
   //         };
-
+ 
   //         this.recognition.start();
-
-
-
-
+ 
+ 
+ 
+ 
   //       } catch (err) {
   //         console.error('Error accessing media devices.', err);
   //       }
   //     },
   //     stopRecording() {
-  //       this.processing=true   
+  //       this.processing=true  
   //       if (this.mediaRecorder && this.mediaRecorder.state !== 'inactive') {
   //         this.mediaRecorder.stop();
   //         this.isRecording = false;
   //       }
-
+ 
   //       if (this.recognition) {
   //         this.recognition.stop();
   //       }
-
+ 
   //     },
-
-
-
+ 
+ 
+ 
   //     async uploadRecording() {
-
+ 
   //       if(this.chatJson.length > 0){
   //         this.chathistoryjsonString = JSON.stringify(this.chatJson);
   //       }else{
   //         this.chathistoryjsonString = JSON.stringify("");
   //       }
-
+ 
   //       if (!this.audioBlob) return;
-
+ 
   //       const formData = new FormData();
   //       formData.append('file', this.audioBlob, 'audio.wav');
   //       formData.append('chathistory', this.chathistoryjsonString);
-
+ 
   //       try {
   //         const response = await COMMON_API_HTTP.post('/transcribe/', formData, {
   //           headers: {
@@ -521,22 +575,22 @@ export default {
   //         });
   //         this.processing=false
   //         console.log(response)
-
-
+ 
+ 
   //         this.transcription = response.data.transcription;
   //         this.translatedtext = response.data.translatedtext;
   //         this.openai = response.data.openai;
   //         this.detectedlanguage=response.data.detectedlanguage;
   //         this.convertedtext=response.data.convertedtext
-
-
-
-
+ 
+ 
+ 
+ 
   //         console.log(this.openai)
   //         this.openairesponse = this.openai.split('\n\n')
   //         console.log(this.openairesponse)
-
-
+ 
+ 
   //         this.chatJson.push({
   //         transcription: this.transcription,
   //         translatedtext: this.translatedtext,
@@ -545,17 +599,17 @@ export default {
   //         convertedtext: this.convertedtext,
   //         openairesponse: this.openairesponse,
   //       });
-
+ 
   //       // this.playAudio(this.convertedtext,this.detectedlanguage)
   // console.log(this.chatJson)
-
+ 
   //       } catch (error) {
   //         this.processing=false
   //         console.error('Error uploading audio file.', error);
   //       }
   //     },
-
-
+ 
+ 
   //     async playAudio(text,detectedlanguage) {
   //       try {
   //       console.log("play audio : " + text)
@@ -563,7 +617,7 @@ export default {
   //         console.log('Please enter some text');
   //         return;
   //       }
-
+ 
   //       if (this.isPaused && this.speechSynthesisUtterance) {
   //             window.speechSynthesis.resume();
   //             this.isPaused = false;
@@ -571,63 +625,63 @@ export default {
   //           }
   //           // this.play=false
   //           // this.pause=true;
-
-
-
-
+ 
+ 
+ 
+ 
   //    // Create SpeechSynthesisUtterance for each segment
   //       const speechSynthesisUtterance = new SpeechSynthesisUtterance(text);
-
+ 
   //       // Specify the language attribute
   //       // if(detectedlanguage.includes("Korean")){
   //       // speechSynthesisUtterance.lang = 'ko-KR'; // Korean language
   //       // }
-
-
-
+ 
+ 
+ 
   //       const audioContext = new (window.AudioContext || window.webkitAudioContext)();
   //       const mediaStreamDestination = audioContext.createMediaStreamDestination();
   //       const mediaRecorder = new MediaRecorder(mediaStreamDestination.stream);
   //       let audioChunks = [];
-
+ 
   //       mediaRecorder.ondataavailable = (event) => {
   //         audioChunks.push(event.data);
   //       };
-
+ 
   //       mediaRecorder.onstop = () => {
   //         const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
   //         this.audioUrl = URL.createObjectURL(audioBlob);
   //       };
-
+ 
   //       mediaRecorder.start();
   //       window.speechSynthesis.speak(speechSynthesisUtterance);
-
+ 
   //       speechSynthesisUtterance.onend = () => {
   //         mediaRecorder.stop();
   //       };
-
-
+ 
+ 
   //   } catch (error) {
   //         console.log(error)
   //       //  CreateToaster('No Data Available', '', 'warning');
   //       }
-
+ 
   //     },
-
-
+ 
+ 
   //     pauseAudio() {
-
+ 
   //           if (window.speechSynthesis.speaking && !this.isPaused) {
   //             window.speechSynthesis.pause();
-
+ 
   //             this.isPaused = true;
   //           } else if (this.isPaused) {
   //             window.speechSynthesis.resume();
-
+ 
   //           }
-
+ 
   //         },
-
+ 
   //         stopAudio() {
   //           window.speechSynthesis.cancel();
   //           if (this.mediaRecorder && this.mediaRecorder.state === 'recording') {
@@ -635,12 +689,12 @@ export default {
   //           }
   //           this.isPaused = false;
   //         }
-
-
+ 
+ 
   //   }
 };
 </script>
-
+ 
 <style scoped>
 button {
   margin: 5px;
